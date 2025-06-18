37b9a0ed0611029a8b663abcdabd7f4b40166502
David Heppenheimer <david@heppify.de>
2025-06-18 09:19:53 +0200

    forgot to check isAdmin oopsie

--- src/app/api/admin/addproduct/route.ts
+++ src/app/api/admin/addproduct/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma as db } from "@/prisma";
+import { checkAdmin } from "@/lib/checkAdmin";
 
 export async function POST(request: NextRequest) {
+  const isAdmin = await checkAdmin();
+  if (!isAdmin) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const data = await request.json();
   const images = data.images.map((image: string) => ({ url: image }));
 
